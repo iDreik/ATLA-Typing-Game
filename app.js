ca11dd179b57c76da3ef2d63ac92f92c983ffa80
Enoc Elias <eeliasbr1@gmail.com>
2023-06-13 12:53:14 -0500

    v0.1.8 rudimentary dynamic body

--- app.js
+++ app.js
@@ -12,31 +12,31 @@ Vue.createApp({
   data() {
     return {
       quotes: [
-        { id: 0, text: "There is nothing wrong with letting people who love you, help you.", byWho: "- Iroh" },
-        { id: 1, text: "While it is always best to believe in oneself, a little help from others can be a great blessing.", byWho: "- Iroh" },
-        { id: 2, text: "It is usually best to admit mistakes when they occur, and to seek to restore honor.", byWho: "- Iroh" },
-        { id: 3, text: "It's time for you to look inward and begin asking yourself the big question: who are you and what do you want?", byWho: "- Iroh" },
-        { id: 4, text: "You must never give in to despair. Allow yourself to slip down that road and you surrender to your lowest instincts.", byWho: "- Zuko" },
-        { id: 5, text: "Life happens wherever you are, whether you make it or not.", byWho: "- Iroh" },
-        { id: 6, text: "Sharing tea with a fascinating stranger is one of life's true delights.", byWho: "- Iroh" },
-        { id: 7, text: "The greatest illusion of this world is the illusion of separation. Things you think are separate and different are actually one and the same.", byWho: "- Guru Pathik" },
-        { id: 8, text: "It is important to draw wisdom from many different places. If you take it from only one place, it becomes rigid and stale.", byWho: "- Iroh" },
-        { id: 9, text: "Failure is only the opportunity to begin again. Only this time, more wisely.", byWho: "- Iroh" },
-        { id: 10, text: "Sometimes the best way to solve your own problems is to help someone else.", byWho: "- Iroh" },
-        { id: 11, text: "The Fire Nation and the Avatar are linked, but now the Avatar is the last hope for the world.", byWho: "- Roku" },
-        { id: 12, text: "As the Avatar, you are not meant to hurt others, but to bring peace.", byWho: "- Roku" },
-        { id: 13, text: "I know now that no one can give you your honor. It's something you earn for yourself by choosing to do what's right.", byWho: "- Zuko" },
-        { id: 14, text: "In the end, we will remember not the words of our enemies, but the silence of our friends.", byWho: "- Roku" },
-        { id: 15, text: "When we hit our lowest point, we are open to the greatest change.", byWho: "- Roku" },
-        { id: 16, text: "Destiny is a funny thing. You never know how things are going to work out.", byWho: "- Aang" },
-        { id: 17, text: "Fire is the element of power. The people of the Fire Nation have desire and will, and the energy and drive to achieve what they want.", byWho: "- Iroh" },
-        { id: 18, text: "The true mind can weather all the lies and illusions without being lost. The true heart can touch the poison of hatred without being harmed.", byWho: "- Lion Turtle" },
-        { id: 19, text: "You have light and peace inside of you. If you let it out, you can change the world around you.", byWho: "- Iroh" },
-        { id: 20, text: "The greatest victories are those won without fighting.", byWho: "- Iroh" },
-        { id: 21, text: "In the darkest times, hope is something you give yourself. That is the meaning of inner strength.", byWho: "- Iroh" },
-        { id: 22, text: "Sometimes life is like this dark tunnel, you can't always see the light at the end of the tunnel, but if you just keep moving, you will come to a better place.", byWho: "- Iroh" },
-        { id: 23, text: "Some friendships are so strong, they can even transcend lifetimes.", byWho: "- Katara" },
-        { id: 24, text: "Water is the element of change. The people of the Water Tribe are capable of adapting to many things.", byWho: "- Katara" },
+        { id: 0, text: "There is nothing wrong with letting people who love you, help you.", byWho: "- Iroh", affinity: "fire"},
+        { id: 1, text: "While it is always best to believe in oneself, a little help from others can be a great blessing.", byWho: "- Iroh", affinity: "fire"},
+        { id: 2, text: "It is usually best to admit mistakes when they occur, and to seek to restore honor.", byWho: "- Iroh", affinity: "fire"},
+        { id: 3, text: "It's time for you to look inward and begin asking yourself the big question: who are you and what do you want?", byWho: "- Iroh", affinity: "fire"},
+        { id: 4, text: "You must never give in to despair. Allow yourself to slip down that road and you surrender to your lowest instincts.", byWho: "- Zuko", affinity: "fire"},
+        { id: 5, text: "Life happens wherever you are, whether you make it or not.", byWho: "- Iroh", affinity: "fire"},
+        { id: 6, text: "Sharing tea with a fascinating stranger is one of life's true delights.", byWho: "- Iroh", affinity: "fire"},
+        { id: 7, text: "The greatest illusion of this world is the illusion of separation. Things you think are separate and different are actually one and the same.", byWho: "- Guru Pathik", affinity: "spirit"},
+        { id: 8, text: "It is important to draw wisdom from many different places. If you take it from only one place, it becomes rigid and stale.", byWho: "- Iroh", affinity: "fire"},
+        { id: 9, text: "Failure is only the opportunity to begin again. Only this time, more wisely.", byWho: "- Iroh", affinity: "fire"},
+        { id: 10, text: "Sometimes the best way to solve your own problems is to help someone else.", byWho: "- Iroh", affinity: "fire"},
+        { id: 11, text: "The Fire Nation and the Avatar are linked, but now the Avatar is the last hope for the world.", byWho: "- Roku", affinity: "fire"},
+        { id: 12, text: "As the Avatar, you are not meant to hurt others, but to bring peace.", byWho: "- Roku", affinity: "fire"},
+        { id: 13, text: "I know now that no one can give you your honor. It's something you earn for yourself by choosing to do what's right.", byWho: "- Zuko", affinity: "fire"},
+        { id: 14, text: "In the end, we will remember not the words of our enemies, but the silence of our friends.", byWho: "- Roku", affinity: "fire"},
+        { id: 15, text: "When we hit our lowest point, we are open to the greatest change.", byWho: "- Roku", affinity: "fire"},
+        { id: 16, text: "Destiny is a funny thing. You never know how things are going to work out.", byWho: "- Aang", affinity: "air"},
+        { id: 17, text: "Fire is the element of power. The people of the Fire Nation have desire and will, and the energy and drive to achieve what they want.", byWho: "- Iroh", affinity: "fire"},
+        { id: 18, text: "The true mind can weather all the lies and illusions without being lost. The true heart can touch the poison of hatred without being harmed.", byWho: "- Lion Turtle", affinity: "spirit"},
+        { id: 19, text: "You have light and peace inside of you. If you let it out, you can change the world around you.", byWho: "- Iroh", affinity: "fire"},
+        { id: 20, text: "The greatest victories are those won without fighting.", byWho: "- Iroh", affinity: "fire"},
+        { id: 21, text: "In the darkest times, hope is something you give yourself. That is the meaning of inner strength.", byWho: "- Iroh", affinity: "fire"},
+        { id: 22, text: "Sometimes life is like this dark tunnel, you can't always see the light at the end of the tunnel, but if you just keep moving, you will come to a better place.", byWho: "- Iroh", affinity: "fire"},
+        { id: 23, text: "Some friendships are so strong, they can even transcend lifetimes.", byWho: "- Katara", affinity: "water"},
+        { id: 24, text: "Water is the element of change. The people of the Water Tribe are capable of adapting to many things.", byWho: "- Katara", affinity: "water"},
       ],
 
       imageIndex: 0,
@@ -54,6 +54,7 @@ Vue.createApp({
       userInputted: false,
       randomQuote: null,
       randomQuoteByWho: "",
+      randomQuoteAffinity: "",
       correctlyTypedWords: [],
 
       startTime: null,
@@ -71,6 +72,8 @@ Vue.createApp({
       highScoreList: {},
 
       isLoading: true,
+
+      isDefaultBody: true,
     };
   },
   computed: {
@@ -115,6 +118,7 @@ Vue.createApp({
       this.randomQuote = this.quotes[randomIndex].text.split("").map(char => ({char: char, isDefault: true}));
       this.currentQuoteId = this.quotes[randomIndex].id;
       this.randomQuoteByWho = this.quotes[randomIndex].byWho;
+      this.randomQuoteAffinity = this.quotes[randomIndex].affinity;
       this.userInput = "";
       this.correctlyTypedWords = [];
       this.$nextTick(() => {
@@ -251,6 +255,24 @@ Vue.createApp({
         sessionStorage.setItem("sessionHighScoreList", JSON.stringify(newVal));
       },
       deep: true,
+    },
+    randomQuoteAffinity(newVal) {
+      document.body.classList.remove("fireBody", "waterBody", "airBody", "earthBody", "spiritBody");
+      if (newVal === "fire") {
+        document.body.classList.add("fireBody");
+      }
+      if (newVal === "water") {
+        document.body.classList.add("waterBody");
+      }
+      if (newVal === "air") {
+        document.body.classList.add("airBody");
+      }
+      if (newVal === "earth") {
+        document.body.classList.add("earthBody");
+      }
+      if (newVal === "spirit") {
+        document.body.classList.add("spiritBody");
+      }
     },
   },
 }).mount("#typingGame");
